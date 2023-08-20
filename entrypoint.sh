d02ba312989f95e0d8016548d330b6b3c4eecd6f
Jonas Kaninda <jonaskaninda@gmail.com>
2023-08-20 02:24:16 +0200

    Add count number of files on mounting

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "$ACCESS_KEY:$SECRET_KEY" | tee /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
 echo "Mounting Object storage in /s3mnt .... "
 s3fs $BUCKETNAME /s3mnt -o passwd_file=/etc/passwd-s3fs -o use_cache=/tmp/s3cache -o allow_other -o url=$S3_ENDPOINT -o use_path_request_style
-
+ls /s3mnt | wc -l
 fi
 
 bash
